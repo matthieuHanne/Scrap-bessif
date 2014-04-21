1f8de9d02d5cc5096f1c61d28f8ce3d251398c86
hanser <benedict.hanser@gmail.com>
2014-04-22 00:13:11 +0200

    zbra

--- ballzy/app.py
+++ ballzy/app.py
@@ -12,9 +12,9 @@ def init():
 @app.route('/scrap', methods=['POST'])
 def scrap():
     keywords = request.form['keywords'].split()
-	export_config = request.form['export_config'].split()
+	export_config = request.form['export_config'].split()# inventeur / demandeur / classification_internationalle/ abregé_pour / mot_clef (k)
 	mail_addr= request.form['mail_to']
-	databases = request.form['databases'].split()
+	databases = request.form['databases'].split() # f pour espace.net.fr et w pour espace.net.world , faire comme pour export config 
     scrap_bessif_launcher.Scrap(mail_addr,export_config,keywords,databases)
     return 'test:)'
 if __name__ == '__main__':
