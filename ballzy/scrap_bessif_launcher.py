c4101af960999994fc3fc3294d63aee253ff6114
hanser <benedict.hanser@gmail.com>
2014-04-21 22:45:24 +0200

    zbra

---
+++ ballzy/scrap_bessif_launcher.py
@@ -0,0 +1,27 @@
+import scrap_espace_fr
+import scrap_espace_word
+import export_to_xml
+import mail
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+
+
+def Scrap(mail,export_arg,keywords,databases):
+	for i in databases : 
+		if i=='f':
+			print "googo"
+			scrap_espace_fr.scrap_multi_keyword(keywords)
+		elif i =='w':
+			scrap_espace_word.scrap_multi_keyword(keywords)
+	export_to_xml.export(export_arg)
+	mail.send_mail(mail,keywords)
+
+mail = "benedict.hanser@gmail.com"
+export = "i"
+keywords=["zob"]
+databases = "w"
+
+Scrap(mail,export,keywords,databases)
+	
