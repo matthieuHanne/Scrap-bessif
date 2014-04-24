6d2df6a16fad3bdf9358987ab54555eee07cdbee
hanser <benedict.hanser@gmail.com>
2014-04-25 01:24:46 +0200

    zbra

--- ballzy/app.py
+++ ballzy/app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask import request 
 import scrap_bessif_launcher
+import json
 
 app = Flask(__name__)
 
@@ -11,11 +12,26 @@ def init():
 
 @app.route('/scrap', methods=['POST'])
 def scrap():
-    keywords = request.form['keywords'].split()
-	export_config = request.form['export_config'].split()# inventeur / demandeur / classification_internationalle/ abregé_pour / mot_clef (k)
+	keywords = request.form['keywords'].split()
+	export_config = request.form['export_config'].split()
 	mail_addr= request.form['mail_to']
-	databases = request.form['databases'].split() # f pour espace.net.fr et w pour espace.net.world , faire comme pour export config 
-    scrap_bessif_launcher.Scrap(mail_addr,export_config,keywords,databases)
-    return 'test:)'
+	databases = request.form['databases'].split() 
+	scrap_bessif_launcher.Scrap(mail_addr,export_config,keywords,databases)
+	return 'test:)'
+
+@app.route('/export', methods=['POST'])
+def export():
+	export_config = request.form['export_config'].split()
+	return json.dumps(scrap_bessif_launcher.GetGlobalDatabase(export_config,1))
+
+@app.route('/number', methods=['POST'])
+def getnumber():
+	keywords = request.form['keywords'].split()
+	return json.dumps(scrap_bessif_launcher.GetNumber(keywords))
+
+@app.route('/keyword', methods=['GET'])
+def GetKeyword():
+	return json.dumps(scrap_bessif_launcher.GetKeywordsFromGlobalDatabase())
+	 
 if __name__ == '__main__':
     app.run()
